280893574dbd080db8f4f8fc67ffcc1faa6d86ce
Juergen <myemail>
2021-09-10 14:51:41 +0200

    Add pass after zone pass

--- R/plot_passes_between_areas.R
+++ R/plot_passes_between_areas.R
@@ -9,7 +9,7 @@ plot_passes_between_areas <- function(df,
   soccerPitch(arrow = spielrichtung,
               title = title,
               subtitle = subtitle) +
-    geom_line(data = df, aes(x = x, y = y, group = Zeit, color = Zeit), arrow = arrow(angle = 30, length = unit(0.05, "inches"), ends = "last", type = "closed")) +
+    geom_path(data = df, aes(x = x, y = y, group = Zeit, color = Zeit), arrow = arrow(angle = 30, length = unit(0.05, "inches"), ends = "last", type = "closed")) +
     geom_rect(data=pos1, mapping=aes(xmin=x1, xmax=x2, ymin=y1, ymax=y2), color="red", alpha=0.05) +
     geom_rect(data=pos2, mapping=aes(xmin=x1, xmax=x2, ymin=y1, ymax=y2), color="darkgreen", alpha=0.05)
 
