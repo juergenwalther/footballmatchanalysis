5eda6272a54ece135dc8482063c588feafb17415
Juergen <myemail>
2021-09-15 09:26:16 +0200

    Fix if list element contains no data

--- R/obtain_amount_passes.R
+++ R/obtain_amount_passes.R
@@ -1,17 +1,21 @@
 obtain_amount_passes <- function(df, continued = TRUE) {
-  if (continued == TRUE) {
-    res <- df %>%
-      dplyr::group_by(Zeit) %>%
-      dplyr::filter(n() > 2) %>%
-      dplyr::ungroup() %>%
-      dplyr::select(Zeit) %>%
-      dplyr::distinct() %>%
-      summarise(n())
+  if (is.null(df)) {
+    return(0)
   } else {
-    res <- df %>%
-      dplyr::select(Zeit) %>%
-      dplyr::distinct() %>%
-      summarise(n())
+    if (continued == TRUE) {
+      res <- df %>%
+        dplyr::group_by(Zeit) %>%
+        dplyr::filter(n() > 2) %>%
+        dplyr::ungroup() %>%
+        dplyr::select(Zeit) %>%
+        dplyr::distinct() %>%
+        summarise(n())
+    } else {
+      res <- df %>%
+        dplyr::select(Zeit) %>%
+        dplyr::distinct() %>%
+        summarise(n())
+    }
+    return(res)
   }
-  return(res)
 }
