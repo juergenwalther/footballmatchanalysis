86550cf949ac9e852ee644b5a9ec92dbabbf630f
Juergen <myemail>
2021-09-23 05:13:32 +0200

    Refine plots

--- R/get_won_space_pass_sequence.R
+++ R/get_won_space_pass_sequence.R
@@ -1,12 +1,13 @@
-get_won_space_pass_sequence <- function(pass) {
+get_won_space_pass_sequence <- function(pass, area1 = 35, area2 = 70, area3 = 105) {
   pass %>%
+    mutate(area = ifelse(x < area1, 1, ifelse(x < area2, 2, 3))) %>%
     group_by(Zeit) %>%
     slice(-n()) %>%
     mutate(
       space_won = (last(x) - first(x))/105*100,
       total = n()
     ) %>%
-    dplyr::select(Zeit, space_won, total) %>%
+    dplyr::select(Zeit, area, space_won, total) %>%
     filter(space_won != 0) %>%
     slice(1)
 }
--- R/plot_won_space.R
+++ R/plot_won_space.R
@@ -1,32 +1,44 @@
 plot_won_space <- function(pass, histogram = FALSE, outpath, filename) {
   if (histogram == FALSE) {
     # Basic scatter plot
-    ggplot(pass, aes(x = space_won, y = total, color = space_won)) +
-      geom_point(shape = 16, size = 5, show.legend = FALSE) +
+    ggplot(pass, aes(x = space_won, y = total, color = as.factor(area))) +
+      geom_point(shape = 16, size = 5) +
       # geom_smooth(method=lm, show.legend = FALSE) +
       theme_minimal() +
-      scale_color_gradient(low = "#0091ff", high = "#f0650e") +
+      #scale_color_gradient(low = "#0091ff", high = "#f0650e") +
       xlim(-50, 80) +
       ylim(0, 15) +
       labs(
         title = "Raumgewinn vs Anzahl der Pässe in Stafette",
         x = "Raumgewinn (in % von Spielfeldlänge)",
         y = "Anzahl Pässe in Stafette"
-      )
+      ) +
+      scale_color_manual(name = "Spielfeld Drittel",
+                         values = c("1" = "red",
+                                    "2" = "green",
+                                    "3" = "blue"),
+                         labels = c("defensiv", "mitte", "offensiv"))
 
 
     ggsave(file = paste0(outpath, "/", filename, ".png"))
   }
   else {
-    ggplot(pass, aes(x = space_won)) +
-      geom_histogram(breaks = c(-75,-50,-25,0,25,50,75,100), color = "black", fill = "blue") +
+    ggplot(pass, aes(x = space_won, y=..count../sum(..count..), fill = as.factor(area))) +
+      #geom_bar(aes(y = (..count..)/sum(..count..)), breaks = c(-75,-50,-25,0,25,50,75,100), color = "black", fill = "blue") +
+      #scale_y_continuous(formatter = 'percent') +
+      geom_histogram(breaks = c(-75,-50,-25,0,25,50,75,100)) +
       theme_minimal() +
       labs(
         title = "Raumgewinn im Histogram",
         x = "Raumgewinn (in % von Spielfeldlänge)",
-        y = "Anzahl Passstafetten"
+        y = "Anteil Passstafetten an Gesamtzahl von Passstafetten"
       ) +
-      ylim(0, 50)
+      ylim(0, 0.5) +
+      scale_fill_manual(name = "Spielfeld Drittel",
+                         values = c("1" = "red",
+                                    "2" = "green",
+                                    "3" = "blue"),
+                         labels = c("defensiv", "mitte", "offensiv"))
 
     ggsave(file = paste0(outpath, "/", filename, ".png"))
   }
