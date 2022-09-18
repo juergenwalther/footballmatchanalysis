51df50e3a096aa5a54e3427d73c0be662dc1eea9
Juergen <myemail>
2022-09-18 16:48:16 +0200

    Add sum of parameters

--- R/tracktics_single_player_summary.R
+++ R/tracktics_single_player_summary.R
@@ -15,10 +15,20 @@ tracktics_single_player_summary <- function(data_single) {
     }
   }
 
+  sum_numeric <- function(x) {
+    if (length(x[!is.na(x)]) > 0) {
+      round(sum(as.numeric(x), na.rm = T), digits = 1)
+    } else {
+      NA
+    }
+  }
+
   if (is.null(dim(data_single))) {
-    ret <- as.numeric(data_single)
+    ret <- cbind(rep(NA, length(data_single)),rep(NA, length(data_single)))
   } else {
-    ret <- apply(data_single, 1, mean_numeric)
+    sums <- apply(data_single, 1, sum_numeric)
+    sums[c(2,9,10,11,18:22)] <- NA
+    ret <- cbind(sums, apply(data_single, 1, mean_numeric))
   }
   return(ret)
 }
