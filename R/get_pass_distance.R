dbc283e443c2f6b1dbf7d3996b8f434487a58353
Juergen <myemail>
2023-01-26 22:07:12 +0100

    First adjustments

--- R/get_pass_distance.R
+++ R/get_pass_distance.R
@@ -4,6 +4,16 @@
 #' @export
 #'
 #' @examples
-get_pass_distance <- function(){
+get_pass_distance <- function(pass){
+  pass_distance <- pass %>% group_by(Zeit) %>%
+    slice(c(2:pmax(2, n() - 1))) %>%
+    ungroup() %>%
+    mutate() %>%
 
 }
+
+
+euclidean_dist <- function(x, y) sqrt(sum((x - y)^2))
+x = pass$x[1]
+
+sqrt(sum((x - y)^2))
