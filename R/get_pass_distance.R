2f4417250fe87fb555f28127b44339c660523bc4
Juergen <myemail>
2023-02-04 21:36:06 +0100

    Calculate pass distance

--- R/get_pass_distance.R
+++ R/get_pass_distance.R
@@ -8,12 +8,7 @@ get_pass_distance <- function(pass){
   pass_distance <- pass %>% group_by(Zeit) %>%
     slice(c(2:pmax(2, n() - 1))) %>%
     ungroup() %>%
-    mutate() %>%
+    mutate(dis = c(0,sqrt(diff(x)^2 + diff(y)^2)))
 
+  return(pass_distance)
 }
-
-
-euclidean_dist <- function(x, y) sqrt(sum((x - y)^2))
-x = pass$x[1]
-
-sqrt(sum((x - y)^2))
