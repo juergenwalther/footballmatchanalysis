955105f8adcacca8f12f67885f81316f12c65f2e
Juergen <myemail>
2023-04-20 20:58:36 +0200

    Make manual tracktics lean

--- R/tracktics_read_manual_csv.R
+++ R/tracktics_read_manual_csv.R
@@ -9,9 +9,8 @@
 tracktics_read_manual_csv <- function(file) {
   ret <- read.csv(file = file)
 
-  colnames(ret) <- c("Spiel", "Anteil an hoher Intensität an Gesamtdistanz (in %)", "Anteil von Sprinten an Gesamtdistanz (in %)", "Anteil von Rennen an Gesamtdistanz (in %)",
-                    "Anteil Spiel im vorderen Drittel", "Tempowechsel insgesamt", "Anteil Tempowechsel Intensiv (in %)", "Anteil Tempowechsel Sprint (in %)",
-                    "Anteil Tempowechsel Rennen (in %)", "Anzahl Sprints in Spielrichtung", "Anzahl Sprints gegen Spielrichtung", "Mittlere Sprintlänge (in m)"
+  colnames(ret) <- c("Spiel", "Anteil von Sprinten an Gesamtdistanz (in %)", "Anteil von Rennen an Gesamtdistanz (in %)",
+                    "Anteil Spiel im vorderen Drittel", "Anzahl Sprints in Spielrichtung"
   )
 
   return(ret)
